f481077988339db1d5d3746dd7d6b05e7569d8f2
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-11-11 02:47:01 +0000

    hurr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,28 @@
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+//    ----------------------------------------------------------------------
+//    Copyright © 2024, 2025
+//                Jiang Jie
+//    Copyright © 2025
+//                Pellegrino Prevete
+//
+//    All rights reserved
+//    ----------------------------------------------------------------------
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
@@ -6,6 +31,6 @@ export default tseslint.config(
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   { ignores: [
-    'dist',
+    'dist'
     ], }
 );
